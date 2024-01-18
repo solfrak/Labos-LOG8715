379b551cfd86315ce4a91ba0beb05be81c4942fb
samuel Laporte <laporte.samuel2000@gmail.com>
2024-01-18 12:08:06 -0500

    Init tp1

---
+++ TP1_H24/Assets/RegisterSystems.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class RegisterSystems
+{
+    public static List<ISystem> GetListOfSystems()
+    {
+        // determine order of systems to add
+        var toRegister = new List<ISystem>();
+        
+        // Add your systems here
+
+        return toRegister;
+    }
+}
